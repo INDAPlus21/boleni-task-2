91a21a8cd3a2e98ccb745c2bf985b82dc692ce0a
Jonatan Bölenius <boleni@kth.se>
2021-09-15 14:33:20 +0200

    Added functioning version of a_different_problem, but doesn't work on kattis

--- a_different_problem/src/main.rs
+++ a_different_problem/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    
+
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("There was something wrong with the input.");
+
+        let numbers = input.trim().split(" ").collect::<Vec<&str>>();
+        let n1 = numbers[0].parse::<i64>().expect("Please input a valid number.");
+        let n2 = numbers[1].parse::<i64>().expect("Please input a valid number.");
+
+        println!("{}",
+        if n1-n2 > 0 { // There are problably built-in features for abs value but eh-
+            n1-n2
+        } else {
+            (n1-n2)*-1
+        });
+    }
 }
